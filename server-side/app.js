cecba4892ed0c3951b86d92c4bc0a302ded65929
Shuvo Deb <shuvodeb17014@gmail.com>
2023-07-24 11:19:17 +0600

    create review api

--- server-side/app.js
+++ server-side/app.js
@@ -98,6 +98,13 @@ async function run() {
             res.send(result)
         })
 
+        app.get('/review/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await admissionCollection.findOne(query)
+            res.send(result)
+        })
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("database is connected");
